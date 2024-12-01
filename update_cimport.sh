f18457a014cb8ff4d36d4c94088a6ad092a8222d
Lukas Lalinsky <llalinsky@live365.com>
2024-12-01 21:00:26 +0100

    More reliable way of patching circular definitions

--- update_cimport.sh
+++ update_cimport.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+cp vendor/libuv/include/uv.h vendor/libuv/include/uv.h.orig
+
+perl -pi -e 's{^  uv_close_cb close_cb}{  void* close_cb}g' vendor/libuv/include/uv.h
+perl -pi -e 's{^  uv_read_cb read_cb}{  void* read_cb}g' vendor/libuv/include/uv.h
+
 zig translate-c -target aarch64-linux -lc -Ivendor/libuv/include vendor/libuv/include/uv.h > src/cimport_linux.zig 
-patch -p1 < cimport_linux_fix.patch
+zig translate-c -target aarch64-macos -lc -Ivendor/libuv/include vendor/libuv/include/uv.h > src/cimport_macos.zig 
+
+mv vendor/libuv/include/uv.h.orig vendor/libuv/include/uv.h
